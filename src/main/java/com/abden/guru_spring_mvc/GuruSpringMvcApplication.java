4e8433ad734508643e76cf4624aab979e970c672
mohammedAbden <mohamed.abdelfattah@channelvas.com>
2021-10-08 00:24:11 +0200

    init commit

---
+++ src/main/java/com/abden/guru_spring_mvc/GuruSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.abden.guru_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruSpringMvcApplication.class, args);
+    }
+
+}
